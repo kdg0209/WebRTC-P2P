aeef5e78d3e9298c403b934f98dcd00d66be2850
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-04-27 22:32:42 +0900

    Update server.js

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,15 +1,14 @@
-import express from "express";
-import fs from "fs";
-import http from "http";
-import cors from "cors";
-import { socketInit } from "./socket/socket.js";
+import express from 'express';
+import cors from 'cors';
+import logger from './logger/logger.js';
+import { socketInit } from './socket/socket.js';
 const app = express();
-const server = http.createServer(app);
+let server;
 const PORT = 3001;
 
 app.use(cors());
 
-socketInit(server);
 server = app.listen(PORT, () => {
-    logger.debug(`[HTTP] Soda Server is started on port : ` + PORT);
+  logger.debug(`[HTTP] Soda Server is started on port : ` + PORT);
 });
+socketInit(server);
